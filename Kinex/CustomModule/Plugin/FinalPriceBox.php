0335c17be30e29e8f8d128e0d6d763c15e5efca3
KumkumArora <kumkum@infino.co>
2023-08-16 09:53:04 +0530

    added custom fee module

--- Kinex/CustomModule/Plugin/FinalPriceBox.php
+++ Kinex/CustomModule/Plugin/FinalPriceBox.php
@@ -4,7 +4,7 @@ namespace Kinex\CustomModule\Plugin;
 class FinalPriceBox
 {
     public function afterToHtml(\Magento\Catalog\Pricing\Render\FinalPriceBox $subject,  $result)
-    {   
+    {    
         if($subject->getSaleableItem()->getData('product_select_attribute') == 1){
 
             if($subject->getSaleableItem()->getData('Product_availability') != 0)
